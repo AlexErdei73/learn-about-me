26c149d63d82e01e70c1ad76d898f8e94da8c6d4
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-09-01 09:48:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# LEARN ABOUT ME
+
+It is a mini social media app, which has been made in the book Express In Action
+by Evan M. Hahn. The book is a great one, but a bit old. I have updated the code
+with the newest versions of the libraries and used the alternatives instead of
+depricated ones.
+
+## Installation
+
+1. Install MongoDB for your system from
+   [MongoDB installation](https://www.mongodb.com/docs/manual/administration/install-community/)
+   The database needs to be listening on localhost:27017/test .
+2. Clone the repository
+3. `cd learn-about-me` and `npm i` to install the dependancies.
+4. `npm start` will start the app.js file, which is an Express app.
+5. Open with your browser the [](http://localhost:3000).
---
+++ models/user.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var SALT_FACTOR = 10;
+
+var userSchema = mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    displayName: String,
+    bio: String
+    });
+
+userSchema.methods.name = function() {
+    return this.displayName || this.username;
+    };
+
+userSchema.pre("save", function(done) {
+    var user = this;
+    if (!user.isModified("password")) {
+        return done();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) { return done(err); }
+        bcrypt.hash(user.password, salt, function(err, hashedPassword) {
+            if (err) { return done(err); }
+            user.password = hashedPassword;
+            done();
+        });
+    });
+});
+
+userSchema.methods.checkPassword = function(guess, done) {
+    bcrypt.compare(guess, this.password, function(err, isMatch) {
+        done(err, isMatch);
+    });
+};
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
